d6e8676a1499e217172f5bac2eca33eed1de4f65
Henrique Bortoletto <bortolettohenrique@gmail.com>
2023-03-21 21:15:56 -0300

    feat(post): 🚀 creating the first header styles and comments for the post component 🚀

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -2,10 +2,44 @@ import S from './styles.module.css'
 
 function Post(props) {
   return (
-    <>
-      <strong>{props.author}</strong>
-      <p>{props.content}</p>
-    </>
+    <section className={S.post}>
+      <div className={S.header}>
+        <div className={S.avatar}>
+          <div className={S.thumb}>
+            <img
+              src="https://github.com/henriquebortoletto.png"
+              alt="Imagem de avatar do usuario"
+              loading="lazy"
+            />
+          </div>
+          <div className={S.info}>
+            <strong>Henrique Bortoletto</strong>
+            <p>Software Engineer</p>
+          </div>
+        </div>
+        <time
+          dateTime="2022-05-11 08:13:30"
+          title="11 de Maio às 08:13h"
+        >
+          Públicado há 1h
+        </time>
+      </div>
+
+      <div className={S.content}>
+        <p>Fala galeraa 👋</p>
+        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+        <p>👉&nbsp;<a href="https://github.com/henriquebortoletto" target="_blank">github.com/henriquebortoletto</a></p>
+        <p>
+          <span>#novoprojeto</span>
+          <span>#nlw</span>
+          <span>#rocketseat</span>
+        </p>
+      </div>
+
+      <div>
+        ...comentarios
+      </div>
+    </section>
   )
 }
 
